d56004536767c0cfea6fdba5f00d73dab22c83bb
36mbMonster <jonathan@bt053-0614.cob.sjsu.edu>
2014-07-23 09:41:21 -0700

    worked on structures.

--- recordstructs.h
+++ recordstructs.h
@@ -1,28 +1,39 @@
 #pragma once
 
-#define FACULTY fac_st;
-#define COURSE course_stl;
+#define FACULTY struct fac_st;
+#define COURSE struct course_stl;
+#define SECTION struct section_stl;
 
 struct fac_st
 {
     char *firstname;
     char *middleitl;
     char *lastname;
+
+    char* title;
+    char* dept;
+    int fulltime;
+
+    char* office_bldg;
+    short room;
+}
+
+struct section_stl
+{
+    char number[3];
+    char building[3];
+
+    short room;
+
+    FACULTY *instructor;
+
+    short stime;
+    short etime;
 }
 
 struct course_stl
 {
     char *dept;
-    char number[3];
-    char building[3];
-
-    short room;
-    short section
-
-    FACULTY *instructor;
-}
-
-struct section
-{
+    SECTION *sections;
 
 }
