75a14e9913e2ed86423ed898753ce918b16db017
Jon Vaccaro <jonathan.vaccaro@sjsu.edu>
2014-07-29 08:27:13 -0700

    messing around with trees and lists.

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <gtk/gtk.h>
 
+//function declarations
+void show_about_dialog();
+
 GtkBuilder *builder;
-GObject *window;
+GtkWidget *window;
+GtkAboutDialog *about_dialog;
+
+GtkWidget *treeview;
+GtkCellRenderer *renderer;
+GtkTreeViewColumn *column;
+GtkListStore *store;
+
+GObject *about_item;
 
 int main(int argc, char *argv[])
 {
@@ -11,14 +22,44 @@ int main(int argc, char *argv[])
 	builder = gtk_builder_new();
 	gtk_builder_add_from_file(builder, "main_win.glade", NULL);
 
-	window = gtk_builder_get_object(builder,"window1");
-	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    gtk_window_set_title(GTK_WINDOW(window), "Test");
+	//Load windows
+	window = GTK_WIDGET(gtk_builder_get_object(builder,"window"));
+	about_dialog = GTK_ABOUT_DIALOG(gtk_builder_get_object(builder,"aboutdialog"));
 
-    
+	//Load menu items
+	about_item = gtk_builder_get_object(builder, "about");
+
+	//Load other stuff
+	treeview = gtk_builder_get_object(builder, "treeview");
+
+	//TEST TEST TEST TEST
+	  renderer = gtk_cell_renderer_text_new();
+  column = gtk_tree_view_column_new_with_attributes("List Items",renderer, "text", 0, NULL);
+  gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), column);
+
+  store = gtk_list_store_new(1, G_TYPE_STRING);
+
+  gtk_tree_view_set_model(GTK_TREE_VIEW(treeview),
+      GTK_TREE_MODEL(store));
+
+	//Connect signals
+	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	g_signal_connect(about_item, "activate", G_CALLBACK(show_about_dialog), NULL);
 
     gtk_widget_show(window);
     gtk_main();
 
     return 0;
+}
+/**
+***************************************************************************
+*							Signal Functions						  	  *
+***************************************************************************
+*/
+
+void show_about_dialog()
+{
+	//gtk_widget_show_all( about_dialog );
+	gtk_dialog_run(GTK_DIALOG(about_dialog));
+	gtk_widget_hide(GTK_WIDGET(about_dialog));
 }
