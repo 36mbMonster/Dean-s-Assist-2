36c1c4fa495c342e4f2175627ce7c76c6ba378b3
Jonathan Vaccaro <jonathan@Jefferson.(none)>
2014-07-28 19:04:16 -0700

    Rendered the gui through the C code for the very first time.

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
 #include <gtk/gtk.h>
 
+GtkBuilder *builder;
+GObject *window;
+
 int main(int argc, char *argv[])
 {
-    GtkWidget *window;
 
-    gtk_init(&argc, &argv);
+	gtk_init(&argc, &argv);
 
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	builder = gtk_builder_new();
+	gtk_builder_add_from_file(builder, "main_win.glade", NULL);
+
+	window = gtk_builder_get_object(builder,"window1");
+	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     gtk_window_set_title(GTK_WINDOW(window), "Test");
 
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    
 
     gtk_widget_show(window);
     gtk_main();
