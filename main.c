11cd17887099f423959195666e231b0360089318
Jon Vaccaro <jonathan.vaccaro@sjsu.edu>
2014-08-04 15:17:59 -0700

    Figured out how to show/hide columns. Added an option in the menu to configure that.

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <gtk/gtk.h>
-
+#include <stdio.h>
+#include <string.h>
 //function declarations
 void show_about_dialog();
+
+int i = 0;
 
 GtkBuilder *builder;
 GtkWidget *window;
@@ -9,7 +12,7 @@ GtkAboutDialog *about_dialog;
 
 GtkWidget *treeview;
 GtkCellRenderer *renderer;
-GtkTreeViewColumn *column;
+GtkTreeViewColumn *columns[9];
 GtkListStore *store;
 
 GObject *about_item;
@@ -30,17 +33,30 @@ int main(int argc, char *argv[])
 	about_item = gtk_builder_get_object(builder, "about");
 
 	//Load other stuff
-	treeview = gtk_builder_get_object(builder, "treeview");
+	//treeview = gtk_builder_get_object(builder, "treeview");
+	treeview = gtk_tree_view_new();
 
 	//TEST TEST TEST TEST
-	  renderer = gtk_cell_renderer_text_new();
-  column = gtk_tree_view_column_new_with_attributes("List Items",renderer, "text", 0, NULL);
-  gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), column);
+	renderer = gtk_cell_renderer_text_new();
 
-  store = gtk_list_store_new(1, G_TYPE_STRING);
+	for(i = 1; i < 10; i++)
+	{
+		char test[20] = "treeviewcolumn";
+		char buffer[2];
+		sprintf(buffer, "%d", i);
+		strcat(test,buffer);
 
-  gtk_tree_view_set_model(GTK_TREE_VIEW(treeview),
-      GTK_TREE_MODEL(store));
+		columns[i-1] = gtk_builder_get_object(builder, test);
+	}
+
+	//column = gtk_tree_view_column_new_with_attributes("List Items",renderer, "text", 0, NULL);
+	//gtk_tree_view_column_set_visible (columns[0],FALSE);
+  	gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), columns[4]);
+
+  	//store = gtk_list_store_new(1, G_TYPE_STRING);
+
+  	//gtk_tree_view_set_model(GTK_TREE_VIEW(treeview),
+	//GTK_TREE_MODEL(store));
 
 	//Connect signals
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
