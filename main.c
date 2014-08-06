2d2d397b1770e35a7ca57c3b7a509f939930eaa9
Jon Vaccaro <jonathan.vaccaro@sjsu.edu>
2014-08-06 16:46:58 -0700

    breaking the tree model :P

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 void show_about_dialog();
 void show_adjust_columns_dialog();
 void hide_adjust_columns_dialog();
+void new_column();
+char *get_string_from_form();
 
 int i = 0;
 
@@ -14,8 +16,10 @@ GtkWidget *window;
 GtkWidget *show_hide_columns_window;
 GtkAboutDialog *about_dialog;
 
-GtkWidget *treeview;
+GtkWidget*treeview;
 GtkTreeModel *model;
+GtkTreeModel *filter;
+GtkTreePath *path;
 GtkCellRenderer *renderer;
 GtkTreeViewColumn *columns[9];
 GtkListStore *store;
@@ -24,6 +28,7 @@ GtkTreeSelection *selector;
 GObject *about_item;
 GObject *quit_item;
 GObject *show_hide_columns_item;
+GObject *new_course_item;
 
 int main(int argc, char *argv[])
 {
@@ -42,14 +47,33 @@ int main(int argc, char *argv[])
 	about_item = gtk_builder_get_object(builder, "about");
 	quit_item = gtk_builder_get_object(builder, "quit");
 	show_hide_columns_item = gtk_builder_get_object(builder, "show_hide_columns");
+	new_course_item = gtk_builder_get_object(builder, "new_course");
 
 	//Load other stuff
-	treeview = gtk_tree_view_new();
-	selector = gtk_builder_get_object(builder, "treeview-selection");
-	model = gtk_builder_get_object(builder,);
+	treeview = GTK_WIDGET(gtk_builder_get_object(builder, "treeview"));
+	selector = GTK_TREE_SELECTION(gtk_builder_get_object(builder, "treeview-selection"));
+	store = GTK_LIST_STORE(gtk_builder_get_object(builder,"liststore"));
+
+	//Set the tree model
+	path = gtk_tree_path_new_from_indices(1,-1);
+	model = gtk_tree_model_filter_new(GTK_TREE_MODEL(store), path);
+	gtk_tree_view_set_model(GTK_TREE_VIEW(treeview), model);
 
 	//Get the columns from the builder and populate the column array.
 	renderer = gtk_cell_renderer_text_new();
+
+    /*store = gtk_list_store_new( 1, G_TYPE_INT );
+    for( i = 1; i < 4; i++ )
+    {
+        GtkTreeIter parent_iter;
+
+        gtk_list_store_append( store, &parent_iter);
+        gtk_list_store_set( store, &parent_iter, 0, i, -1 );
+
+    }
+
+    path = gtk_tree_path_new_from_indices(1,-1);
+    filter = gtk_tree_model_filter_new(GTK_TREE_MODEL(store), path);*/
 
 	for(i = 1; i < 10; i++)
 	{
@@ -63,26 +87,26 @@ int main(int argc, char *argv[])
 
 	//column = gtk_tree_view_column_new_with_attributes("List Items",renderer, "text", 0, NULL);
 	//gtk_tree_view_column_set_visible (columns[0],FALSE);
-  	gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), columns[4]);
+  	//gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), columns[4]);
 
   	//store = gtk_list_store_new(1, G_TYPE_STRING);
-
-  	//gtk_tree_view_set_model(GTK_TREE_VIEW(treeview),
-	//GTK_TREE_MODEL(store));
+  	//gtk_tree_view_set_model(GTK_TREE_VIEW(treeview),GTK_TREE_MODEL(store));
 
 	//Connect signals
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 	g_signal_connect(about_item, "activate", G_CALLBACK(show_about_dialog), NULL);
 	g_signal_connect(quit_item, "activate", G_CALLBACK(gtk_main_quit), NULL);
+	g_signal_connect(new_course_item, "activate", G_CALLBACK(new_column), NULL);
 	g_signal_connect(show_hide_columns_item, "activate", G_CALLBACK(show_adjust_columns_dialog), NULL);
 	g_signal_connect(show_hide_columns_window, "hide", G_CALLBACK(hide_adjust_columns_dialog), NULL);
+
 
 	//Connect column signals
 	//for(i = 1; i < 10; i++)
 	//{
-	//	g_signal_connect(selector, "changed", G_CALLBACK(show_about_dialog), NULL);
+		g_signal_connect(selector, "changed", G_CALLBACK(get_string_from_form), NULL);
 	//}
-	gtk_tree_model_get()
+	//gtk_tree_model_get()
 
     gtk_widget_show(window);
     gtk_main();
@@ -110,4 +134,27 @@ void show_adjust_columns_dialog()
 void hide_adjust_columns_dialog()
 {
 	gtk_widget_hide(GTK_WIDGET(show_hide_columns_window));
+}
+
+char *get_string_from_form()
+{
+	gchar *data;
+	GtkTreeIter iter;
+
+	gtk_tree_store_append( GTK_TREE_STORE(store), &iter, NULL );
+
+	gtk_tree_model_get(model, &iter, &data);
+
+	printf("%s",data);
+	return data;
+}
+
+void new_column()
+{
+	GtkTreeIter iter;
+	gtk_tree_model_get_iter(model, &iter, path);
+
+	gtk_list_store_insert(store, &iter, 1);
+
+	gtk_list_store_set(store, &iter, 1, "TEST");
 }
