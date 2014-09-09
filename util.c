27a5363b9459577919dda6272dd5b0d0f2a33194
Jon Vaccaro <jonathan.vaccaro@sjsu.edu>
2014-09-09 15:04:51 -0700

    This should have been pushed yesterday. See previous commit message.

--- util.c
+++ util.c
@@ -2,6 +2,8 @@
 
 int is_int(const gchar *str);
 
+const char *COURSE_NUMBER_ERROR = "The entry was invalid. A course number can be two or three didgets with an optional letter at the end.";
+
 int is_int(const gchar *str)
 {
     char *err;
