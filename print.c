c88b9d577c4ba14d91de6e4b84db9587dce2458b
Jon Vaccaro <jonathan.vaccaro@sjsu.edu>
2015-02-18 17:20:37 -0800

    Fleshed out the print initialization function some more. There is an issue where it interprets any pressed button as a cancel signal.

--- print.c
+++ print.c
@@ -9,23 +9,39 @@ GtkPrintSettings *print_settings;
 
 void start_printer(GtkWidget *window)
 {
+	GError *error;
 	GtkPrintOperation *print_operation;
+	print_settings = NULL;
 
 	print_operation = gtk_print_operation_new();
 
 	if(print_settings != NULL)
 		gtk_print_operation_set_print_settings(print_operation, print_settings);
 
-	g_signal_connect(print_operation, "begin_print", G_CALLBACK(begin_print),NULL);
-	g_signal_connect(print_operation, "draw_page", G_CALLBACK(draw_page),NULL);
+	g_signal_connect(print_operation, "begin-print", G_CALLBACK(begin_print),NULL);
+	g_signal_connect(print_operation, "draw-page", G_CALLBACK(draw_page),NULL);
 
 	int res;
-	res = gtk_print_operation_run (print_operation, GTK_PRINT_OPERATION_ACTION_PRINT_DIALOG,GTK_WINDOW (window), NULL);
+	res = gtk_print_operation_run (print_operation, GTK_PRINT_OPERATION_ACTION_PRINT_DIALOG,GTK_WINDOW (window), &error);
 
-	if(res == GTK_PRINT_OPERATION_RESULT_APPLY)
+	if(res == GTK_PRINT_OPERATION_RESULT_ERROR)
 	{
-		printf("placeholder");
+		GtkWidget *error_dialog;
+		error_dialog = gtk_message_dialog_new(GTK_WINDOW (window), GTK_DIALOG_DESTROY_WITH_PARENT, GTK_MESSAGE_ERROR, GTK_BUTTONS_CLOSE, "The following error occured:\n%s",error->message);
 	}
+
+	else if(res == GTK_PRINT_OPERATION_RESULT_APPLY)
+	{
+		if(print_settings != NULL)
+			g_object_unref(print_settings);
+		print_settings =  g_object_ref(gtk_print_operation_get_print_settings(print_operation));
+	}
+	else
+	{
+		printf("wrong signal: %d\n",res);
+	}
+
+	g_object_unref(print_operation);
 }
 
 void begin_print()
