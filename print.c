e4967af5f4761f5ba3da03f2d576e1274cad65af
Jon Vaccaro <jonathan.vaccaro@sjsu.edu>
2015-02-18 18:27:36 -0800

    More work on printing functions. Pressing 'print' on the dialog now results in successfully sending a signal to the printer and printing a blank page. :smile:

--- print.c
+++ print.c
@@ -4,6 +4,7 @@ void init_print();
 void start_printer(GtkWidget *window);
 void begin_print();
 void draw_page();
+void end_print();
 
 GtkPrintSettings *print_settings;
 
@@ -12,7 +13,6 @@ void start_printer(GtkWidget *window)
 	GError *error;
 	GtkPrintOperation *print_operation;
 	print_settings = NULL;
-
 	print_operation = gtk_print_operation_new();
 
 	if(print_settings != NULL)
@@ -28,6 +28,9 @@ void start_printer(GtkWidget *window)
 	{
 		GtkWidget *error_dialog;
 		error_dialog = gtk_message_dialog_new(GTK_WINDOW (window), GTK_DIALOG_DESTROY_WITH_PARENT, GTK_MESSAGE_ERROR, GTK_BUTTONS_CLOSE, "The following error occured:\n%s",error->message);
+		g_signal_connect(error_dialog, "response", G_CALLBACK(gtk_widget_destroy), NULL);
+		gtk_widget_show(error_dialog);
+		g_error_free(error);
 	}
 
 	else if(res == GTK_PRINT_OPERATION_RESULT_APPLY)
@@ -36,20 +39,28 @@ void start_printer(GtkWidget *window)
 			g_object_unref(print_settings);
 		print_settings =  g_object_ref(gtk_print_operation_get_print_settings(print_operation));
 	}
-	else
-	{
-		printf("wrong signal: %d\n",res);
-	}
 
 	g_object_unref(print_operation);
 }
 
-void begin_print()
+void begin_print(GtkPrintOperation *operation,
+               GtkPrintContext   *context,
+               gpointer           user_data)
 {
+	printf("begin\n");
 
+	gtk_print_operation_set_n_pages(operation,1);
 }
 
-void draw_page()
+void draw_page(GtkPrintOperation *operation,
+			GtkPrintContext *context,
+			gint page,
+			gpointer user_data)
 {
+	printf("draw\n");
+}
 
+void end_print()
+{
+	printf("printing ended.\n");
 }
